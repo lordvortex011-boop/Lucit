ebe8ed68e86b4904cabb02e1821b649d53b2868e
Ayush Singh <96430661+ayush1305@users.noreply.github.com>
2026-09-10 22:19:47 +0530

    Add PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./components/ThemeToggle";
+import posthog from "posthog-js";
 
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
